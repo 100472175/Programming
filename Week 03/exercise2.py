a8509a13fdd1078b8ce56999fe8d6053d23211ff
edusombra <83667153+edusombra@users.noreply.github.com>
2022-09-01 02:05:48 +0200

    Tab tab tab

--- Week 03/exercise2.py
+++ Week 03/exercise2.py
@@ -6,5 +6,13 @@ print (action)
 #"What happens is it gives out a NameError: name 'action' is not defined, because you asigned it an empty value"
 #I didn't know if this is whaten from the exercice, so I also just entered a variable without doing anything else
 """
-a =
 #There is no result because the ariabke has not been initialized
+print("how many times do you want to print the word 'hello'?")
+#number: int
+number = int(input())
+#count: int
+count = 0
+#while count < number:
+while count < number:
+    print("hello")
+    count = count + 1
